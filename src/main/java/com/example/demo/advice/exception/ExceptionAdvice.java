da06ab6bffb8b9e635125c01cea72b67663ce9c7
Eric <ksh117@kakao.com>
2022-08-31 22:10:26 +0900

    fix : Exception advice 메소드명 변경

--- src/main/java/com/example/demo/advice/exception/ExceptionAdvice.java
+++ src/main/java/com/example/demo/advice/exception/ExceptionAdvice.java
@@ -36,19 +36,19 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(EmailDuplicatedException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public CommonResult communicationException(HttpServletRequest request, EmailDuplicatedException e) {
+    public CommonResult emailDuplicatedException(HttpServletRequest request, EmailDuplicatedException e) {
         return responseService.getFailResult(Integer.parseInt(ExceptionEnum.DUPLICATED_MEMBER_EMAIL.getCode()), ExceptionEnum.DUPLICATED_MEMBER_EMAIL.getMessage());
     }
 
     @ExceptionHandler(NameDuplicatedException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public CommonResult communicationException(HttpServletRequest request, NameDuplicatedException e) {
+    public CommonResult nameDuplicatedException(HttpServletRequest request, NameDuplicatedException e) {
         return responseService.getFailResult(Integer.parseInt(ExceptionEnum.DUPLICATED_MEMBER_NAME.getCode()), ExceptionEnum.DUPLICATED_MEMBER_NAME.getMessage());
     }
 
     @ExceptionHandler(RoleNotSetException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public CommonResult communicationException(HttpServletRequest request, RoleNotSetException e) {
+    public CommonResult roleNotSetException(HttpServletRequest request, RoleNotSetException e) {
         return responseService.getFailResult(Integer.parseInt(ExceptionEnum.ROLE_NOT_SET.getCode()), ExceptionEnum.ROLE_NOT_SET.getMessage());
     }
 
