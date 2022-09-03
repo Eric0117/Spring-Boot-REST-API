b58704794cc5859abab3f6e8accaf8bdd09d6871
Eric <ksh117@kakao.com>
2022-09-03 23:48:46 +0900

    feat : Post 컨트롤러 생성

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.jwt.CurrentUser;
+import com.example.demo.config.jwt.UserPrincipal;
+import com.example.demo.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author Eric
@@ -13,4 +17,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @Operation(summary = "Get Post List", description = "Get Post list")
+    @GetMapping
+    public void getPosts() {
+
+    }
+
+    @Operation(summary = "Get Post", description = "Get Post by id")
+    @GetMapping("/{id}")
+    public void getPostById(@PathVariable Long id) {
+
+    }
+
+    @Operation(summary = "Add Post", description = "Add member")
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public void addPost(@CurrentUser UserPrincipal currentUser) {
+
+    }
+
+    @Operation(summary = "Update Post", description = "Update Post (If profile belongs to logged in user or logged in user is admin)")
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public void updatePost(@PathVariable Long id, @CurrentUser UserPrincipal currentUser) {
+
+    }
+
+    @Operation(summary = "Delete Post", description = "Delete Post (If profile belongs to logged in user or logged in user is admin)")
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public void deletePost(@PathVariable Long id, @CurrentUser UserPrincipal currentUser) {
+
+    }
 }
