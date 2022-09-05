75d65aaf58ee59c6df7a6e3dbf47875578e96311
Eric <ksh117@kakao.com>
2022-09-05 21:55:06 +0900

    feat : Comment 엔티티 수정

--- src/main/java/com/example/demo/model/comment/Comment.java
+++ src/main/java/com/example/demo/model/comment/Comment.java
@@ -1,10 +1,10 @@
 package com.example.demo.model.comment;
 
+import com.example.demo.config.BooleanToYNConverter;
 import com.example.demo.model.common.BaseEntity;
 import com.example.demo.model.member.Member;
 import com.example.demo.model.post.Post;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -15,8 +15,10 @@ import javax.validation.constraints.NotBlank;
  * @Since 22. 8. 31.
  **/
 @Entity
-@Data
+@Builder
+@Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "comment")
 public class Comment extends BaseEntity {
     @Id
@@ -35,4 +37,19 @@ public class Comment extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
+
+    @Convert(converter = BooleanToYNConverter.class)
+    private boolean isActive;
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void deActive() {
+        this.isActive = false;
+    }
+
+    public void updateComment(String detail) {
+        this.detail = detail;
+    }
 }
